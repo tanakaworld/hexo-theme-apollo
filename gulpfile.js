cc6459e085936b51785c401dc38a480cc13cc60e
tanakaworld <yutaro.tanaka.world@gmail.com>
2018-06-15 01:44:05 +0900

    Convert *.scss to *.sass and integrate colors to color.sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 // 一次性编译 Sass 
-gulp.task('sass', function() {
-    return gulp.src('./source/scss/*.scss')
+gulp.task('sass', function () {
+    return gulp.src('./source/sass/*.sass')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(autoprefixer())
         .pipe(gulp.dest('./source/css'));
 });
 
 // 实时编译
-gulp.task('default', ['sass'], function() {
-    gulp.watch('./source/scss/_partial/*.scss', ['sass']);
-    gulp.watch('./source/scss/*.scss', ['sass']);
+gulp.task('default', ['sass'], function () {
+    gulp.watch('./source/sass/_partial/*.sass', ['sass']);
+    gulp.watch('./source/sass/*.sass', ['sass']);
 });
